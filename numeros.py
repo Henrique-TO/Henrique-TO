4a4bb0397697ebba36edd32cf7c26ad89e242775
Henrique-TO <144277732+Henrique-TO@users.noreply.github.com>
2023-09-15 16:53:01 -0300

    Add files via upload

---
+++ numeros.py
@@ -0,0 +1,31 @@
+    #1
+# numero = int(input('digite um numero inteiro'))
+
+# if numero > 0:
+#     print('positivo')
+# elif numero < 0:
+#     print('negativo')
+# else:
+#     print('zero')
+
+    #2
+
+lado1 = int(input('digite uma medida'))
+lado2 = int(input('digite a segunda medida'))
+lado3 = int(input('digite a ultima medida'))
+
+if lado1 == lado2 == lado3:
+    print("triangulo equlatero")
+elif lado1 != lado2 == lado3:
+    print("triangulo isosceles")
+else :
+    print ('triangulo escaleno')
+
+    #3
+
+# ano = int(input('digite um ano'))
+
+# if (ano % 4==0 and ano % 100 !=0):
+#     print ('ano bissexto')
+# else:
+#    print('Ano normal')
